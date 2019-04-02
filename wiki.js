5337f176cb0cadfe918da7a8059b6c76fd98f77b
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-01 20:45:38 -0400

    Storing data and coding to display it

--- wiki.js
+++ wiki.js
@@ -1,8 +1,45 @@
+const form = document.querySelector('.searchForm');
 
-fetch('http://example.com/movies.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  // prevent page from reloading when form is submitted
+  event.preventDefault();
+  
+  const input = document.querySelector('.searchForm-input').value;
+  
+  const searchQuery = input.trim();
+ fetchResults(searchQuery);
+
+}
+
+function fetchResults(searchQuery) {
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+  console.log(endpoint);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// function wikibutton(){
+// var url = 'https://www.wikipedia.org/';
+// var data = {username: 'example'};
+
+// fetch(url, {
+//   method: 'POST', // or 'PUT'
+//   body: JSON.stringify(data), // data can be `string` or {object}!
+//   headers:{
+//     'Content-Type': 'application/json'
+//   }
+// }).then(res => res.json())
+// .then(response => console.log('Success:', JSON.stringify(response)))
+// .catch(error => console.error('Error:', error));
+// }
