f4e954274f19c0446e16ca1d7a61d8b982bd9c6f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-03 17:56:35 -0400

    Updated post results

--- wiki.js
+++ wiki.js
@@ -27,8 +27,21 @@ function displayResults(results) {
   const searchResults = document.querySelector('.searchResults');
   // Remove all child elements
   searchResults.innerHTML = '';
-   results.forEach(result => {
-   });
+
+  // Loop over results array
+  results.forEach(result => {
+   const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
+
+   searchResults.insertAdjacentHTML('beforeend',
+      `<div class="resultItem">
+        <h3 class="resultItem-title">
+          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+        </h3>
+        <span class="resultItem-snippet">${result.snippet}</span><br>
+        <a href="${url}" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
+      </div>`
+    );
+  });
 }
 const form = document.querySelector('.searchForm');
 form.addEventListener('submit', handleSubmit);
